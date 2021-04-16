7438c110429b5b80cad3d17512becd527205c483
Douglas Jenkins <d_jenkinsjr@yahoo.com>
2021-04-15 20:24:58 -0700

    Updated

--- server/models/user.js
+++ server/models/user.js
@@ -8,3 +8,28 @@
 ;===========================================
 */
 
+// const fields linked over
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const UserRoleSchema = require('../schemas/selected-security-question');
+const SelectedSecurityQuestionSchema = require('../schemas/selected-security-question');
+
+// user input fields below
+
+let userSchema = new Schema ({
+    userName: { type: String, required: true, unique: true, dropDups: true },
+    password: { type: String, required: true },
+    firstName: { type: String },
+    lastName: { type: String },
+    phoneNumber: { type: String },
+    address: { type: String },
+    email: { type: String },
+    isDisabled: { type: Boolean, default: false },
+    role: UserRoleSchema,
+    selectedSecurityQuestions: [selectedSecurityQuestions],
+    dateCreated: { type: Date, default: new Date() },
+    dateModified: { type: Date }
+
+}, { collection: 'users' });
+
+module.exports = mongoose.model('User', userSchema);
