bdf63bc772841ded647a0566fda59b4da7fe3d28
Douglas Jenkins <d_jenkinsjr@yahoo.com>
2021-04-17 16:33:55 -0700

    Update

--- server/schemas/selected-security-question.js
+++ server/schemas/selected-security-question.js
@@ -2,8 +2,20 @@
 ============================================
 ; Title:  selected-security-question.js
 ; Author: Professor Krasso
-; Date:   15 April 2021
+; Date:   17 April 2021
 ; Modified by: Karina Alvarez, Douglas Jenkins, Arlix Sorto
 ; Description: Selected Security Question Application
 ;===========================================
 */
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// creates the values to fill out
+
+let selectedSecurityQuestionSchema = new Schema ({
+  questionText: { type: String },
+  answerText: { type: String }
+})
+
+module.exports = selectedSecurityQuestionSchema;
