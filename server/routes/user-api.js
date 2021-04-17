77b15a12fedced966f740ef5828a2e76a1569d1c
Douglas Jenkins <d_jenkinsjr@yahoo.com>
2021-04-16 19:41:32 -0700

    Update

--- server/routes/user-api.js
+++ server/routes/user-api.js
@@ -32,22 +32,22 @@ router.get('/', async (req, res) => {
   try {
     //Retrieve all users data.
     User.find({}).where('isDisabled').equals(false).exec(function(err, users){
-      
+
       //Check for errors
       if(err){
         console.log(err);
         const findAllMongodbErrorResponse = new ErrorResponse(500, 'Internal server error', err);
         res.status(500).send(findAllMongodbErrorResponse.toObject());
       }
-      //If no error, send the users object and response*/ 
+      //If no error, send the users object and response*/
       else{
         console.log(users);
         const findAllUsersResponse = new BaseResponse(200, 'Query successful', users);
         res.json(findAllUsersResponse.toObject());
       }
     });
-  } 
-  
+  }
+
   catch (error) {
     console.log(error);
     const findAllCatchErrorResponse = new ErrorResponse (500, 'Internal Server Error', error);
@@ -91,6 +91,88 @@ router.get('/:id', async(req, res) => {
  * UpdateUser API
  * http://localhost:3000/api/users/:id
  */
+router.put('/:id', async (req, res) => {
+  try {
+
+    User.findOne({'_id': req.params.id }, function(err, user){
+
+      // Checking for errors
+      if(err){
+        console.log(err);
+        const updateUserMongodbErrorResponse = new ErrorResponse(500, 'Internal server error', err);
+        res.status(500).send(updateUserMongodbErrorResponse.toObject());
+      }
+
+      else{
+        console.log(user);
+
+        // the following fields allow you update them
+        user.set({
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          phoneNumber: req.body.phoneNumber,
+          address: req.body.address,
+          email: req.body.email
+        })
+
+        // saves the updates
+        user.save(function(err, savedUser){
+          if (err) {
+            console.log(err);
+            const saveUserMongodbErrorResponse = new ErrorResponse(500, 'Internal server error', err);
+            res.status(500).send(saveUserMongodbErrorResponse.toObject());
+          } else {
+            console.log(savedUser);
+            const saveUserResponse = new BaseResponse(200, 'Query Successful', user);
+            res.json(saveUserResponse.toObject());
+          }
+
+        })
+
+      }
+
+    });
+  }
+
+  catch (error) {
+    console.log(error);
+    const updateUserCatchErrorResponse = new ErrorResponse (500, 'Internal Server Error', error);
+    res.status(500).send(updateUserCatchErrorResponse.toObject());
+  }
+
+});
+
+
+/**
+ * FindById API
+ * http://localhost:3000/api/users/:id
+ */
+router.get('/:id', async(req, res) => {
+  try
+  {
+    //this is how we are filtering the data
+    User.findOne({'_id': req.params.id}, function(err, user) {
+      if (err) {
+        console.log(err);
+        const findByIdMongodbErrorResponse = new ErrorResponse(500, 'MongoDB native error', err);
+        res.status(500).send(findByIdMongodbErrorResponse.toObject());
+      }
+      //If no error message
+      else {
+        console.log(user);
+        const findByIdResponse = new BaseResponse(200, 'Query Successful', user);
+        res.json(findByIdResponse.toObject());
+      }
+    })
+  }
+  catch (e)
+  {
+    console.log(e);
+    const findByIdCatchErrorResponse = new ErrorResponse (500, 'Internal Server Error', e);
+    res.status(500).send(findByIdCatchErrorResponse.toObject());
+  }
+});
+
 
 /**
  * DeleteUser API
