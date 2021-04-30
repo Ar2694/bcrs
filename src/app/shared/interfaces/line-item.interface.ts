d7db004ce4cd5eedac0f210f3b70bbadce88056c
Arlix Sorto <Ar2694@outlook.com>
2021-04-30 08:49:26 -0500

    line-item interface ts added

---
+++ src/app/shared/interfaces/line-item.interface.ts
@@ -0,0 +1,13 @@
+/*
+============================================
+; Title: line-item.interface.ts
+; Author: Professor Krasso
+; Date: 30 April 2021
+; Modified by: Arlix Sorto
+; Description: Line-item interface
+;===========================================
+*/
+export interface LineItem{
+    title: string;
+    price: number;
+}
