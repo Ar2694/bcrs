05b4d3088e433303021b359b0b22309c0326bf0a
Arlix Sorto <Ar2694@outlook.com>
2021-04-30 07:39:45 -0500

    Role guard ts file added.

---
+++ src/app/shared/guards/role.guard.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { RoleService } from '../services/role.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RoleGuard implements CanActivate {
+  constructor(private router: Router, private cookiesService, private roleService: RoleService){}
+  
+  //Determine if the user is admin otherwise navigate other users to home page.
+  canActivate(route: ActivatedRouteSnapshot,state: RouterStateSnapshot) {
+
+    return this.roleService.findUserRole(this.cookiesService.get('sessionuser')).pipe(map(res=>{
+      console.log(res);
+      //Check if user is admin
+      if(res['data'].role === 'admin'){
+        return true;
+      }
+      //Otherwise navigate other user to home page.
+      else{
+        this.router.navigate(['/']);
+        return false;
+      }
+    }));
+ 
+  }
+  
+}
