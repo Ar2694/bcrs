ff52c01e33c3da3bf6b93948d9c8ca4a455bb813
Douglas Jenkins <d_jenkinsjr@yahoo.com>
2021-04-30 20:43:13 -0700

    Update

--- src/app/shared/invoice-summary-dialog/invoice-summary-dialog.component.ts
+++ src/app/shared/invoice-summary-dialog/invoice-summary-dialog.component.ts
@@ -1,4 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+/*
+============================================
+; Title: invoice-summary
+; Author: Professor Krasso
+; Date:   30 April 2021
+; Modified by: Karina Alvarez
+; Description: Invoice Summary
+;===========================================
+*/
+
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
+import { Invoice } from "../../shared/interfaces/invoice.interface";
 
 @Component({
   selector: 'app-invoice-summary-dialog',
@@ -6,8 +18,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./invoice-summary-dialog.component.css']
 })
 export class InvoiceSummaryDialogComponent implements OnInit {
+//calls over the invoice
+invoice: Invoice
 
-  constructor() { }
+  constructor(private dialogRef: MatDialogRef<InvoiceSummaryDialogComponent>, @Inject(MAT_DIALOG_DATA) data) {
+    this.invoice = data.invoice;
+   }
 
   ngOnInit(): void {
   }
