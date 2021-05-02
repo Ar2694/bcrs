f464c40e51e3bccd571d9a892cb6e76db8133709
Arlix Sorto <Ar2694@outlook.com>
2021-05-02 11:45:39 -0500

    Role create fix

--- src/app/pages/role-create/role-create.component.ts
+++ src/app/pages/role-create/role-create.component.ts
@@ -12,7 +12,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Role } from 'src/app/shared/interfaces/role.interface';
 import { RoleService } from 'src/app/shared/services/role.service';
-import { MatDialogRef } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-role-create',
@@ -23,7 +23,7 @@ export class RoleCreateComponent implements OnInit {
   form: FormGroup;
   
   //Constructor
-  constructor(private dialogRef: MatDialogRef<RoleCreateComponent>, private fb: FormBuilder, private router: Router, private roleService: RoleService) { }
+  constructor( private fb: FormBuilder, private router: Router, private roleService: RoleService) { }
 
   ngOnInit(): void {
     this.form = this.fb.group({
@@ -47,7 +47,7 @@ export class RoleCreateComponent implements OnInit {
   }
 
   cancel(){
-    this.dialogRef.close();
+    this.router.navigate(['/roles']);
   }
 
 }
